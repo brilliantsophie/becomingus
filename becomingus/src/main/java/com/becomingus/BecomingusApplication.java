8c85934022d406cfa3aa0b1730169a6feb384e74
Sojeong <0brilliantsophie1@gmail.com>
2025-03-01 00:21:44 +0900

    becomingus 프로젝트 생성 및 H2 데이터베이스 세팅 완료

---
+++ becomingus/src/main/java/com/becomingus/BecomingusApplication.java
@@ -0,0 +1,13 @@
+package com.becomingus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BecomingusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BecomingusApplication.class, args);
+	}
+
+}
---
+++ becomingus/src/test/java/com/becomingus/BecomingusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.becomingus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BecomingusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
