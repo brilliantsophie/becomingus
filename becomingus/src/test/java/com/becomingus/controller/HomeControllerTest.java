72bee20d2595438025c8aa9eb4ba494fcf23c05a
Sojeong <0brilliantsophie1@gmail.com>
2025-03-01 23:13:23 +0900

    홈화면 생성
     - 반응형으로 제작
     - 테스트코드 작성 완료

---
+++ becomingus/src/test/java/com/becomingus/controller/HomeControllerTest.java
@@ -0,0 +1,24 @@
+package com.becomingus.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(HomeController.class)
+class HomeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void 홈화면_로딩() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("home"))
+                .andExpect(model().attribute("title", "Becoming Us"));
+    }
+}
